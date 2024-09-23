6f5aa42e9d60145a56287b452ce67c16052ee098
Vikas <80855543+t4tz@users.noreply.github.com>
2024-09-23 15:33:20 +0100

    Testing

---
+++ CarbonSplitGo/CarbonSplitGoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarbonSplitGoApp.swift
+//  CarbonSplitGo
+//
+//  Created by Vikas Mackevičius on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct CarbonSplitGoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
